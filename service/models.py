b11238aa35e8a6cc9b724dad7c3c0ddf32a78a40
Maham Arif <mahamarif49@gmail.com>
2023-02-25 08:00:36 +0000

    Added database model for order items

--- service/models.py
+++ service/models.py
@@ -38,6 +38,7 @@ db = SQLAlchemy()
 def init_db(app):
     """ Initializes the SQLAlchemy app """
     Order.init_db(app)
+    OrderItem.init_db(app)
 
 
 class DataValidationError(Exception):
@@ -73,6 +74,8 @@ class Order(db.Model):
     status = db.Column(
         db.Enum(OrderStatus), nullable=False, server_default=(OrderStatus.CONFIRMED.name)
     )
+    items = db.relationship('OrderItem', backref='order',
+                            cascade="all, delete", lazy=True)
     created_on = db.Column(db.Date(), nullable=False, default=date.today())
     updated_on = db.Column(db.Date(), nullable=False, default=date.today())
 
@@ -109,10 +112,14 @@ class Order(db.Model):
 
     def serialize(self):
         """ Serializes an Order into a dictionary """
+        items: list = []
+        for item in self.items:
+            items.append(OrderItem.serialize(item))
         return {
             "id": self.id,
             "customer_id": self.customer_id,
             "status": self.status.name,  # convert enum to string
+            "items": items,
             "created_on": self.created_on.isoformat(),
             "updated_on": self.updated_on.isoformat()
         }
@@ -132,9 +139,14 @@ class Order(db.Model):
                 self.status = getattr(OrderStatus, data["status"])
             else:
                 self.status = OrderStatus.CONFIRMED
+            self.items = []
+            if "items" in data:
+                for item in data["items"]:
+                    self.items.append(OrderItem().deserialize(item))
             self.updated_on = date.today()
         except AttributeError as error:
-            raise DataValidationError("Invalid attribute: " + error.args[0]) from error
+            raise DataValidationError(
+                "Invalid attribute: " + error.args[0]) from error
         except KeyError as error:
             raise DataValidationError(
                 "Invalid Order: missing " + error.args[0]
@@ -198,3 +210,149 @@ class Order(db.Model):
         """
         logger.info("Processing lookup or 404 for order id %s ...", order_id)
         return cls.query.get_or_404(order_id)
+
+
+class OrderItem(db.Model):
+    """
+    Class that represents an Order Item
+
+    This version uses a relational database for persistence which is hidden
+    from us by SQLAlchemy's object relational mappings (ORM)
+    """
+
+    app = None
+
+    ##################################################
+    # Table Schema
+    ##################################################
+
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False, default=1)
+    price = db.Column(db.Double, nullable=False)
+    order_id = db.Column(db.Integer, db.ForeignKey('order.id'))
+    created_on = db.Column(db.Date(), nullable=False, default=date.today())
+    updated_on = db.Column(db.Date(), nullable=False, default=date.today())
+
+    ##################################################
+    # INSTANCE METHODS
+    ##################################################
+
+    def __repr__(self):
+        return f"<Order Item: id={self.id}, order_id={self.order_id}, product_id={self.product_id}, quantity={self.quantity}>"
+
+    def create(self):
+        """
+        Creates an Order Item to the database
+        """
+        logger.info("Creating item of order# %s", self.order_id)
+        self.id = None  # pylint: disable=invalid-name
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        """
+        Updates an Order Item to the database
+        """
+        if not self.id:
+            raise DataValidationError("Update called with empty ID field")
+        logger.info("Saving order item# %s", self.id)
+        db.session.commit()
+
+    def delete(self):
+        """ Removes an Order Item from the data store """
+        logger.info("Deleting order item# %s", self.id)
+        db.session.delete(self)
+        db.session.commit()
+
+    def serialize(self):
+        """ Serializes an Order into a dictionary """
+        return {
+            "id": self.id,
+            "product_id": self.product_id,
+            "quantity": self.quantity,  # convert enum to string
+            "price": self.price,
+            "order_id": self.order_id,
+            "created_on": self.created_on.isoformat(),
+            "updated_on": self.updated_on.isoformat()
+        }
+
+    def deserialize(self, data):
+        """
+        Deserializes an Order from a dictionary
+
+        Args:
+            data (dict): A dictionary containing the resource data
+        """
+        try:
+            if "id" in data:
+                self.id = data["id"]
+            self.product_id = data["product_id"]
+            self.quantity = data["quantity"]
+            self.price = data["price"]
+            if "order_id" in data:
+                self.order_id = data["order_id"]
+            self.updated_on = date.today()
+        except KeyError as error:
+            raise DataValidationError(
+                "Invalid Order: missing " + error.args[0]
+            ) from error
+        except TypeError as error:
+            raise DataValidationError(
+                "Invalid Order: body of request contained bad or no data - "
+                "Error message: " + str(error)
+            ) from error
+        return self
+
+    ##################################################
+    # CLASS METHODS
+    ##################################################
+
+    @classmethod
+    def init_db(cls, app: Flask):
+        """Initializes the database session
+
+        :param app: the Flask app
+        :type data: Flask
+
+        """
+        logger.info("Initializing database")
+        cls.app = app
+        # This is where we initialize SQLAlchemy from the Flask app
+        db.init_app(app)
+        app.app_context().push()
+        db.create_all()  # make our sqlalchemy tables
+
+    @classmethod
+    def all(cls) -> list:
+        """ Returns all of the Order Items in the database """
+        logger.info("Processing all Order Items")
+        return cls.query.all()
+
+    @classmethod
+    def find(cls, item_id: int):
+        """Finds an Order Item by it's ID
+
+        :param item_id: the id of the item to find
+        :type item_id: int
+
+        :return: an instance with the item_id, or None if not found
+        :return type: Order Item
+
+        """
+        logger.info("Processing lookup for item id %s ...", item_id)
+        return cls.query.get(item_id)
+
+    @classmethod
+    def find_or_404(cls, item_id: int):
+        """Find an Order Item by it's id
+
+        :param item_id: the id of the Order Item to find
+        :type item_id: int
+
+        :return: an instance with the item_id, or 404_NOT_FOUND if not found
+        :return type: Order Item
+
+        """
+        logger.info("Processing lookup or 404 for item id %s ...", item_id)
+        return cls.query.get_or_404(item_id)
--- tests/factories.py
+++ tests/factories.py
@@ -3,7 +3,7 @@ Test Factory to make fake objects for testing
 """
 import factory
 from factory.fuzzy import FuzzyChoice, FuzzyDate
-from service.models import Order, OrderStatus
+from service.models import Order, OrderItem, OrderStatus
 from datetime import date
 
 
@@ -18,5 +18,22 @@ class OrderFactory(factory.Factory):
     customer_id = factory.Sequence(lambda n: n)
     status = FuzzyChoice(choices=[
                          OrderStatus.CONFIRMED, OrderStatus.SHIPPED, OrderStatus.DELIVERED, OrderStatus.CANCELLED])
+    created_on = FuzzyDate(date(2008, 1, 1))
+    updated_on = FuzzyDate(date(2008, 1, 1))
+    items = []
+
+
+class OrderItemFactory(factory.Factory):
+    """Creates fake order items that you don't have to feed"""
+
+    class Meta:  # pylint: disable=too-few-public-methods
+        """Maps factory to data model"""
+        model = OrderItem
+
+    id = factory.Sequence(lambda n: n)
+    product_id = factory.Sequence(lambda n: n)
+    quantity = factory.Sequence(lambda n: n)
+    price = factory.Sequence(lambda n: n*100)
+    order_id = factory.Sequence(lambda n: n)
     created_on = FuzzyDate(date(2008, 1, 1))
     updated_on = FuzzyDate(date(2008, 1, 1))
