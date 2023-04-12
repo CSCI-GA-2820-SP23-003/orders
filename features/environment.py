3b7c34a837a132f6144bcf887378ff03178942ee
Bowen Fu <bf2285@nyu.edu>
2023-04-12 01:24:04 -0400

    setup basic bdd test and ui for create

--- features/environment.py
+++ features/environment.py
@@ -5,7 +5,7 @@ from os import getenv
 from selenium import webdriver
 
 WAIT_SECONDS = int(getenv('WAIT_SECONDS', '60'))
-BASE_URL = getenv('BASE_URL', 'http://localhost:8080')
+BASE_URL = getenv('BASE_URL', 'http://localhost:8000')
 
 
 def before_all(context):
