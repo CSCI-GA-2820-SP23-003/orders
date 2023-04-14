cc545f4c6182ed783b345f6943903a523429f22c
Maham Arif <mahamarif49@gmail.com>
2023-04-14 04:23:40 +0000

    environment updated to run application on port 8080

--- features/environment.py
+++ features/environment.py
@@ -5,7 +5,7 @@ from os import getenv
 from selenium import webdriver
 
 WAIT_SECONDS = int(getenv('WAIT_SECONDS', '60'))
-BASE_URL = getenv('BASE_URL', 'http://localhost:8000')
+BASE_URL = getenv('BASE_URL', 'http://localhost:8080')
 
 
 def before_all(context):
